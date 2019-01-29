0bdc8576a200d25b7aeb89187d0774e432e83156
Martin Aastrup Olsen <martin.olsen@leoilab.com>
2019-01-29 11:33:14 +0100

    Add graphiql

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -61,6 +61,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-export-backends (quote (ascii html icalendar latex md odt)))
  '(package-selected-packages
    (quote
     (py-autopep8 material-theme flycheck elpy ein better-defaults))))
