27b88d436c937e9a4bb65e1b9ac28b64fb9c3015
Martin Aastrup Olsen <martin.olsen@leoilab.com>
2020-01-02 14:21:46 +0100

    not so eager line numbering

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -75,6 +75,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(linum-delay t)
+ '(linum-eager nil)
  '(org-export-backends (quote (ascii html icalendar latex md odt)))
  '(package-selected-packages
    (quote
